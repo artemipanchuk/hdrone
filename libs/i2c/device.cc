5994c6696fa73189e6016d03963b16c8eb7d204a
Mike H. Hawley <mikehhawley@gmail.com>
2014-05-09 01:41:38 +0400

    chore(libs/i2c): add I2C::Device class

---
+++ libs/i2c/device.cc
@@ -0,0 +1,121 @@
+#include "device.hh"
+
+/*******************
+ * Local variables *
+ *******************/
+
+uint8_t cache[2];
+
+/*******************
+ * Local functions *
+ *******************/
+
+static void error(const char* status);
+
+/************
+ * Auxiliary
+ */
+
+static void error(const char* status) {
+    printf(status);
+    printf(".\n");
+    printf("Unable to handle error (I2C). Terminating.\n");
+
+    exit(EXIT_FAILURE);    
+}
+
+/**********
+ * Export *
+ **********/
+
+/******
+ * I2C
+ */
+
+void I2C::init_secondary(void) {
+    int fd = open("/dev/mem", O_RDWR|O_SYNC);
+    if(fd < 0)
+        error("Failed to open /dev/mem initializing secondary bus");
+
+    void* map = mmap(
+        0,
+        4096,
+        PROT_READ|PROT_WRITE,
+        MAP_SHARED,
+        fd,
+        I2C_DEVICE_GPIO_BASE
+    );
+
+    if (map == MAP_FAILED)
+        error("Failed to mmap initializing secondary bus");
+
+    volatile unsigned int *address = (volatile unsigned int*) map;
+
+    int reg0 = address[0];
+    int m0   = 0b00000000000000000000111111111111;
+    int s0   = 0b00000000000000000000100100000000;
+    int b0   = reg0 & m0;
+    if (b0 != s0)
+        address[0] = (reg0 & ~m0) | s0;
+
+    int reg2 = address[2];
+    int m2   = 0b00111111000000000000000000000000;
+    int s2   = 0b00100100000000000000000000000000;
+    int b2   = reg2 & m2;
+    if (b2 != s2)
+        address[2] = (reg2 & ~m2) | s2;
+
+    munmap(map, 4096);
+    close(fd);
+}
+
+/**************
+ * I2C::Device
+ */
+
+I2C::Device::Device(char* bus, uint8_t address) {
+    int fd;
+    if((fd = open(bus, O_RDWR)) < 0)
+        error("Failed to open I2C bus");
+
+    if(ioctl(fd, I2C_SLAVE, address) < 0)
+        error("Failed to select device");
+
+    this->bus = bus;
+    this->fd  = fd;
+}
+
+I2C::Device::~Device(void) {    
+    close(this->fd);
+}
+
+void I2C::Device::write(uint8_t address, uint8_t byte) {
+    cache[0] = address;
+    cache[1] = byte;
+    if(::write(this->fd, cache, 2) != 2)
+        printf("Failed to write byte (I2C::Device::write). Ignoring.\n");     
+}
+
+void I2C::Device::write(uint8_t* buffer, uint8_t size) {
+    if(::write(this->fd, buffer, size) != size)
+        printf("Failed to write buffer (I2C::Device::write). Ignoring.\n");     
+}
+
+void I2C::Device::read(uint8_t address, uint8_t* buffer, uint8_t size) {
+    cache[0] = address;
+    if(::write(this->fd, cache, 1) != 1) {
+        printf("Failed to write address (I2C::Device::read). Ignoring.\n");
+
+        buffer = NULL;
+        
+        return;
+    }
+
+    if(::read(this->fd, buffer, size) != size) {
+        printf("Failed to read buffer (I2C::Device::read). Ignoring.\n");
+
+        buffer = NULL;
+        
+        return;
+    }
+}
---
+++ libs/i2c/device.hh
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <linux/i2c-dev.h>
+
+#ifndef I2C_DEVICE_H
+#define I2C_DEVICE_H
+
+#define I2C_DEVICE_ADXL345_I2C_ADDR  0x53
+#define I2C_DEVICE_SRF02_I2C_ADDR    0x70
+#define I2C_DEVICE_BCM2708_PERI_BASE 0x20000000
+#define I2C_DEVICE_GPIO_BASE         (I2C_DEVICE_BCM2708_PERI_BASE + 0x00200000)
+
+namespace I2C {
+    void init_secondary(void);
+
+    class Device {
+    public:
+        Device(char* bus, uint8_t address);
+        ~Device(void);
+
+        void write(uint8_t address, uint8_t byte);
+        void write(uint8_t* buffer, uint8_t size);
+        void read(uint8_t address, uint8_t* buffer, uint8_t size);
+
+        char* bus;
+        int   fd;
+    private:
+    };
+}
+
+#endif
